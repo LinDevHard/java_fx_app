4118467b08bf6b94652871871cf09d49466be2b0
lindevhard <lindevhard@gmail.com>
2019-06-05 14:37:01 +0600

    simple windows on javaFx

--- TraidingPlatform/src/main/java/module-info.java
+++ TraidingPlatform/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.devteam.platformfx{
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.base;
 
     opens org.devteam to javafx.fxml;
     exports org.devteam;
--- TraidingPlatform/src/main/java/org/devteam/MainApp.java
+++ TraidingPlatform/src/main/java/org/devteam/MainApp.java
@@ -1,5 +1,39 @@
 package org.devteam;
 
-class MainApp {
-    //JavaFx code Here
+import javafx.application.Application;
+import javafx.geometry.Orientation;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+
+class MainApp extends Application {
+
+    public static void main(String[] args){
+        launch();
+    }
+
+    @Override
+    public void init() throws Exception {
+        System.out.println("Init App");
+        super.init();
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FlowPane rootNode = new FlowPane(Orientation.VERTICAL, 0, 20);
+
+        Scene scene = new Scene(rootNode, 200, 200);
+
+        Label label = new Label("First application");
+        rootNode.getChildren().add(label);
+
+        stage.setTitle("Firs application on JavaFx");
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
