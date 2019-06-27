70c645531bf20352420cd9f9605a2b5e613b436c
LinDevHard <lindevhard@gmail.com>
2019-06-27 22:00:55 +0600

    Added first candle chart

---
+++ TraidingPlatform/src/main/java/org/devteam/view/charts/CandleStickExtraValues.java
@@ -0,0 +1,31 @@
+package org.devteam.view.charts;
+
+public class CandleStickExtraValues {
+    private double close;
+    private double high;
+    private double low;
+    private double average;
+
+    public CandleStickExtraValues(double close, double high, double low, double average) {
+        this.close = close;
+        this.high = high;
+        this.low = low;
+        this.average = average;
+    }
+
+    public double getClose() {
+        return close;
+    }
+
+    public double getHigh() {
+        return high;
+    }
+
+    public double getLow() {
+        return low;
+    }
+
+    public double getAverage() {
+        return average;
+    }
+}
