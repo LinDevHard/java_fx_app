d4b7c8c607cd5f0ec85754931f4016b5c0f2d4c6
LinDevHard <lindevhard@gmail.com>
2019-06-27 22:15:18 +0600

    added new method

--- TraidingPlatform/src/main/java/org/devteam/data/DataLoader.java
+++ TraidingPlatform/src/main/java/org/devteam/data/DataLoader.java
@@ -1,7 +1,11 @@
 package org.devteam.data;
 
+import pl.zankowski.iextrading4j.api.stocks.Chart;
 import pl.zankowski.iextrading4j.api.stocks.Quote;
+
+import java.util.List;
 
 public interface DataLoader {
     Quote getStockQuote(String symbol);
+    List<Chart> getStockChart(String symbol);
 }
--- TraidingPlatform/src/main/java/org/devteam/data/ServerLoader.java
+++ TraidingPlatform/src/main/java/org/devteam/data/ServerLoader.java
@@ -1,10 +1,13 @@
 package org.devteam.data;
 
+import pl.zankowski.iextrading4j.api.stocks.Chart;
+import pl.zankowski.iextrading4j.api.stocks.ChartRange;
 import pl.zankowski.iextrading4j.api.stocks.Quote;
 import pl.zankowski.iextrading4j.client.IEXCloudClient;
+import pl.zankowski.iextrading4j.client.rest.request.stocks.ChartRequestBuilder;
 import pl.zankowski.iextrading4j.client.rest.request.stocks.QuoteRequestBuilder;
 
-
+import java.util.List;
 
 
 public class ServerLoader implements DataLoader {
@@ -14,6 +17,14 @@ public class ServerLoader implements DataLoader {
     public Quote getStockQuote(String symbol) {
         return client.executeRequest(new QuoteRequestBuilder()
         .withSymbol(symbol)
+        .build());
+    }
+
+    @Override
+    public List<Chart> getStockChart(String symbol) {
+        return client.executeRequest(new ChartRequestBuilder()
+        .withSymbol(symbol)
+                .withChartRange(ChartRange.ONE_MONTH)
         .build());
     }
 }
